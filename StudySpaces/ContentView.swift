f1a851b6aea171dd612876069ce4315b5a6b14ec
Kenny Chi <kennychi@berkeley.edu>
2019-11-19 12:03:11 -0800

    initialize app

--- StudySpaces/ContentView.swift
+++ StudySpaces/ContentView.swift
@@ -4,20 +4,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image("RW7991")
+            /*Image("RW7991")
             .clipShape(Circle())
             .overlay(
                 Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 20)
             .offset(y: -200)
-            .padding(.bottom, -200)
+            .padding(.bottom, -200)*/
             Group {
                 Text("StudySpaces")
                     .font(.title)
                 Text("at UC Berkeley")
                     .font(.subheadline)
             }
-            .foregroundColor(.white)
+            .foregroundColor(.black)
             .offset(y:-180)
             
             
