fd2e4aa17fb7fafa8d676597524afefa6211df5d
Kenny Chi <kennychi@berkeley.edu>
2019-11-19 02:18:27 -0800

    Initialize Application

---
+++ StudySpaces/ContentView.swift
@@ -0,0 +1,34 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("RW7991")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 20)
+            .offset(y: -200)
+            .padding(.bottom, -200)
+            Group {
+                Text("StudySpaces")
+                    .font(.title)
+                Text("at UC Berkeley")
+                    .font(.subheadline)
+            }
+            .foregroundColor(.white)
+            .offset(y:-180)
+            
+            
+            
+            
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
