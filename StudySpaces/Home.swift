d517cd2ad798c683d76c981f685dfda26400b24b
Kenny Chi <kennychi@berkeley.edu>
2019-11-20 00:25:07 -0800

    connected home screen to screen delegate

---
+++ StudySpaces/Home.swift
@@ -0,0 +1,38 @@
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+    VStack {
+        Image("RW7991")
+        .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 20)
+        .offset(y: -200)
+        .padding(.bottom, -200)
+        Group {
+            Text("StudySpaces")
+                .font(.title)
+                //.foregroundColor(Color(red: 0.2, green: 0.3, blue: 0.7, opacity: 1.0))
+                .underline()
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("at UC Berkeley")
+                .font(.subheadline)
+        }
+        .foregroundColor(.white)
+        .offset(y:-180)
+        
+        
+        
+        
+    }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
--- StudySpaces/LoginView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  StudySpaces
-//
-//  Created by Kenny Chi on 11/19/19.
-//  Copyright © 2019 Kenny Chi. All rights reserved.
-//
-
-import Foundation
