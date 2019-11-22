4018413c37977ad113ba316f707384932fc6b64d
Kenny Chi <kennychi@berkeley.edu>
2019-11-21 21:10:22 -0800

    added new place data

---
+++ StudySpaces/Place.swift
@@ -0,0 +1,41 @@
+//
+//  Place.swift
+//  StudySpaces
+//
+//  Created by Kenny Chi on 11/21/19.
+//  Copyright © 2019 Kenny Chi. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Place: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var category: Category
+    var isFavorite: Bool
+    var capacity: Int
+    
+    var locationCoordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+        
+    }
+    enum Category: String, CaseIterable, Codable, Hashable{
+        case library = "Library"
+        case studyroom = "Study Room"
+        case cafe = "Cafe"
+    }
+    var image: Image{
+        ImageStore.shared.image(name: imageName)
+    }
+}
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
---
+++ StudySpaces/PlaceRow.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceRow.swift
+//  StudySpaces
+//
+//  Created by Kenny Chi on 11/21/19.
+//  Copyright © 2019 Kenny Chi. All rights reserved.
+//
+
+import SwiftUI
+import Foundation
+struct PlaceRow: View {
+    var place: Place
+    var body: some View {
+        HStack{
+            place.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(place.name)
+                
+        }
+    }
+}
+
+struct PlaceRow_Previews: PreviewProvider{
+    static var previews: some View{
+        PlaceRow(place: placeData[0])
+    }
+}
